3af486275dea6ca818f8b026c387d8f45616025e
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2026-01-31 18:57:26 +0530

    done

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
-app.use(cors())
+// FORCE HEADERS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/test", (req, res) => {
